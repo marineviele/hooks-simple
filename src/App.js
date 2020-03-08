f3bfd6b0c36fe6701a51cd314b5918b24f3fb4f3
Marina <@marineviele>
2020-03-08 18:16:47 +0000

    feat: MVP completed: on buttons click, list from request is rendered.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
-  state =  {resource:'posts'};
+  state = {
+    resource: 'posts',
+    resources: []
+  };
+
+  componentDidMount() {
+    this.fetchResources();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // ** fetch only if resource changed
+    if (prevState.resource !== this.state.resource) {
+      this.fetchResources();
+    }
+  }
+
+  fetchResources = async () => {
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.state.resource}`
+    );
+
+    this.setState({ resources: res.data });
+  };
+
+  renderNumberOfResources = number => (
+    this.state.resources
+      .filter((cur, index) => index < number)
+      .map(item => <li key={item.id}>{item.title}</li>)
+  );
 
   render() {
     return (
       <div>
-        <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-        <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
-        <div>{this.state.resource}</div>
+        <button onClick={() => this.setState({ resource: 'posts' })}>
+          Posts
+        </button>
+        <button onClick={() => this.setState({ resource: 'todos' })}>
+          Todos
+        </button>
+        <ul>{this.renderNumberOfResources(25)}</ul>
       </div>
     );
   }
-};
+}
 
 export default App;
